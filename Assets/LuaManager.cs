352f4996d2183aff7531827cca003f7ffdb70216
Joleeee <jolejonaseh@gmail.com>
2018-05-17 23:34:53 +0200

    Lua supports drawing sprites getting the r key and getting any axis and getting deltaTime.
    Added
    lua support
    reads the filed named script.lua in the data path.
    in lua:
    spr function for drawing sprites
    getting deltaTime
    getting any axis
    getting the r key
    
    also fixed so sprites show outside editor

---
+++ Assets/LuaManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using MoonSharp.Interpreter;
+using System;
+
+public class LuaManager : MonoBehaviour {
+    public Texture2D texture;
+
+    public static List<LuaSprite> spriteQueue;
+
+    Script script;
+
+	// Use this for initialization
+	void Start () {
+        spriteQueue = new List<LuaSprite>();
+        script = new Script();
+        script.DoString(File.ReadAllText(Application.dataPath + "/script.lua"));
+        script.Globals["spr"] = (Action<int, int>)Spr; //Action if a void
+        script.Globals["GetInput"] = (Func<string, float>)GetInput; //Func if returning a value
+        script.Globals["time"] = (Func<float>)DeltaTime;
+        script.Globals["restart"] = (Action)LoadScript;
+        script.Globals["r"] = (Func<bool>)GetR;
+
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+        spriteQueue.Clear();
+        if (loadScript)
+        {
+            loadScript = false;
+            script = new Script();
+            script.DoString(File.ReadAllText(Application.dataPath + "/script.lua"));
+            script.Globals["spr"] = (Action<int, int>)Spr; //Action if a void
+            script.Globals["GetInput"] = (Func<string, float>)GetInput; //Func if returning a value
+            script.Globals["time"] = (Func<float>)DeltaTime;
+        }
+        
+        script.Call(script.Globals["Update"], Time.deltaTime);
+	}
+
+    bool GetR()
+    {
+        return Input.GetKeyDown(KeyCode.R);
+    }
+
+    bool loadScript = false;
+    void LoadScript()
+    {
+        loadScript = true;
+        script = new Script();
+        script.DoString(File.ReadAllText(Application.dataPath + "/script.lua"));
+        script.Globals["spr"] = (Action<int, int>)Spr; //Action if a void
+        script.Globals["GetInput"] = (Func<string, float>)GetInput; //Func if returning a value
+        script.Globals["time"] = (Func<float>)DeltaTime;
+    }
+
+    float DeltaTime()
+    {
+        return Time.deltaTime;
+    }
+
+    void Spr(int x, int y)
+    {
+        spriteQueue.Add(new LuaSprite(texture, x, y));
+    }
+
+    float GetInput(string axis)
+    {
+        return Input.GetAxisRaw(axis);
+    }
+
+	void OnRenderObject(){
+        foreach (var sprite in spriteQueue)
+        {
+            sprite.Draw();
+        }
+        
+	}
+}
--- Assets/LuaSprite.cs
+++ Assets/LuaSprite.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LuaSprite : MonoBehaviour {
+public class LuaSprite/* : MonoBehaviour*/ {
 
-    public Sprite sprite;
-    public int x, y;
-    
-    public LuaSprite(Sprite sprite, int x, int y)
+    public Texture2D texture;
+    public int scale;
+    public float x;
+    public float y;
+    public Rect screenRect;
+
+    public LuaSprite(Texture2D texture, float x, float y, int scale = 1)
     {
-
+        this.texture = texture;
+        this.x = x;
+        this.y = y;
+        this.scale = scale;
     }
+
+	public void Draw(){
+        screenRect = new Rect(x, y, texture.width, texture.height);
+        Graphics.DrawTexture(screenRect, texture);
+	}
+	
 
 	// Use this for initialization
 	void Start () {
