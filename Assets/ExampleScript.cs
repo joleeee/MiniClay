d32737dc29f2acd2b8ad3a59db5a3106fc43d76c
Jonas Hole <Cvpp7NwQPnSkBCn>
2018-05-17 15:19:03 +0200

    Initial commit

---
+++ Assets/ExampleScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour {
+
+    public Texture texture;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnRenderObject () {
+        Graphics.DrawTexture(new Rect(0, 0, 8, 8), texture);
+	}
+}
---
+++ Assets/LuaSprite.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuaSprite : MonoBehaviour {
+
+    public Sprite sprite;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
